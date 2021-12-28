1021b771da806d5286a15882a52c407994f3c5dd
Jxis <gavrilovicnatasa123@gmail.com>
2021-12-28 13:34:16 +0100

    Crypto API

--- UI/main.py
+++ UI/main.py
@@ -1,11 +1,33 @@
 from flask import Flask, render_template, request, json, session, jsonify, flash
+from requests import Request, Session
 
 app = Flask(__name__)
+
+############### CRYPTO EXTERNAL API ######################
+
+url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
+
+parameters = {
+    'slug':
+    "bitcoin,ethereum,solana,xrp,cardano,avalanche,polkadot,dogecoin,polygon,litecoin,chainlink,tron,stellar,cosmos",
+    'convert': 'USD'
+}
+
+headers = {
+    'Accepts': 'application/json',
+    'X-CMC_PRO_API_KEY':
+    'af2460d3-d55c-4349-8e18-eb2844eb0058'  # napravila poseban acc na coinmarketcap za drs
+}
+session = Session()
+session.headers.update(headers)
+##########################################################
 
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    response = session.get(url, params=parameters)
+    return render_template("home.html",
+                           response=json.loads(response.text)['data'])
 
 
 @app.route('/login')
