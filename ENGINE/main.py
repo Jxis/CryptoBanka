5e4f639259dd276221b3e05bdd8db2aaecef6f10
Stefan Zuparic <stefan99zuparic@hotmail.com>
2021-12-29 14:39:51 +0100

    koneckija na sql oracle bazu

--- ENGINE/main.py
+++ ENGINE/main.py
@@ -1,5 +1,11 @@
 from flask import Flask
 
+import cx_Oracle
+def getConnection() :
+    connection = cx_Oracle.connection("sys/sys@localhost:1512/xe");
+    return connection
+
+
 app = Flask(__name__)
 
 if __name__ == "__main__":
