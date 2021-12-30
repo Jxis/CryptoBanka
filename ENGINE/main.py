f8eb8048857a007f55ddcf6b015f442a81c894eb
Jxis <gavrilovicnatasa123@gmail.com>
2021-12-30 17:18:30 +0100

    Revert "koneckija na sql oracle bazu"
    This reverts commit 5e4f639259dd276221b3e05bdd8db2aaecef6f10.

--- ENGINE/main.py
+++ ENGINE/main.py
@@ -1,11 +1,5 @@
 from flask import Flask
 
-import cx_Oracle
-def getConnection() :
-    connection = cx_Oracle.connection("sys/sys@localhost:1512/xe");
-    return connection
-
-
 app = Flask(__name__)
 
 if __name__ == "__main__":
